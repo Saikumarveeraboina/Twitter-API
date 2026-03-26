e0337aa46974926e6afadcb292cfa098970a123b
Saikumar <saikumar96030433@gmail.com>
2026-03-26 14:48:02 +0530

    Refactor app.js to improve structure and readability

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require("express")
 const app = express(); 
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-
+const jwt = require("jsonwebtoken");  
 app.use(express.json()); 
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
