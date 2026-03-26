24ef01a642407b61b070fcc76ee1a5159bc3ac6d
Saikumar <saikumar96030433@gmail.com>
2026-03-26 14:47:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-app.use(express.json());
+app.use(express.json()); 
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 
